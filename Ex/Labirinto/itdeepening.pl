965fefa2f8fd672a4307da03ee3fc661dc5d037b
altair <luca.barra@edu.unito.it>
2025-02-23 15:13:24 +0100

    deep

--- Ex/Labirinto/itdeepening.pl
+++ Ex/Labirinto/itdeepening.pl
@@ -1,27 +1,23 @@
-% itdeeping
+initialize:-retractall(current_depth(_)),
+            assert(current_depth(1)).
 
-initialize :- 
-    retractall(current_depth(_)), 
-    assert(current_depth(1)).
-
-risolvi(S, [], _, _) :- finale(S), !.
-risolvi(S, [Az | ListaAzioni], Visitati, ProfMax) :-
-    ProfMax > 0,
-    applicabile(Az, S), 
-    trasforma(Az, S, SNuovo),
-    \+ member(SNuovo, Visitati),
-    NuovaProfMax is ProfMax - 1,
-    risolvi(SNuovo, ListaAzioni, [S | Visitati], NuovaProfMax).
-
-prova(Cammino) :-
-    initialize, 
-    iniziale(S0), 
-    current_depth(D), 
-    risolvi(S0, Cammino, [], D). 
-
-prova(Cammino) :-
-    current_depth(D), 
-    DNew is D + 1,
+prova(Cammino):-
+    iniziale(S0),
+    current_depth(D),
+    write('---- Profondita: '),write(D),write('\n'),
+    risolvi(S0,Cammino,[],D).
+prova(Cammino):-
+    current_depth(D),
+    DNew is D+1,
     retractall(current_depth(_)),
     assert(current_depth(DNew)),
     prova(Cammino).
+
+risolvi(S,[],_,_):-finale(S),!.
+risolvi(S,[Az|ListaAzioni],Visitati,ProfMax):-
+    ProfMax > 0,
+    applicabile(Az,S),
+    trasforma(Az,S,Snuovo),
+    \+member(Snuovo,Visitati),
+    NuovaProfMax is ProfMax-1,
+    risolvi(Snuovo,ListaAzioni,[S|Visitati],NuovaProfMax).
